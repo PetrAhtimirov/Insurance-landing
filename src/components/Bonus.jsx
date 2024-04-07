3b4aa560eac91939e62bbaf03cb636ac669a7315
ShmatEugene <shmat.evg@yandex.ru>
2024-04-07 12:34:33 +0300

    поправить тексты, убрать контекст

--- src/components/Bonus.jsx
+++ src/components/Bonus.jsx
@@ -1,54 +1,39 @@
 const Bonus = () => {
     return (
-        <div className="bonus">
-            <div className="bonus__inner container">
-                <h2 className="bonus__title">Наши бонусы</h2>
-                <div className="bonus__content">
-                    <div className="bonus__card">
-                        <h3 className="bonus__card__title">
-                            Розыгрыши
-                        </h3>
-                        <p className="bonus__card__desc">
-                            на билетов на мероприятия
-                            и подарков от партнёров
+        <div className='bonus'>
+            <div className='bonus__inner container'>
+                <h2 className='bonus__title'>Наши преимущества</h2>
+                <div className='bonus__content'>
+                    <div className='bonus__card'>
+                        <h3 className='bonus__card__title'>Розыгрыши</h3>
+                        <p className='bonus__card__desc'>
+                            на билетов на мероприятия и подарков от партнёров
                         </p>
                     </div>
-                    <div className="bonus__card">
-                        <h3 className="bonus__card__title">
-                            Кешбек
-                        </h3>
-                        <p className="bonus__card__desc">
+                    <div className='bonus__card'>
+                        <h3 className='bonus__card__title'>Кешбек</h3>
+                        <p className='bonus__card__desc'>
                             процент от покупки билета возвращается вам
                         </p>
                     </div>
-                    <div className="bonus__card">
-                        <h3 className="bonus__card__title">
-                            Сниженная страховая стоимость
-                        </h3>
-                        <p className="bonus__card__desc">
+                    <div className='bonus__card'>
+                        <h3 className='bonus__card__title'>Сниженная страховая стоимость</h3>
+                        <p className='bonus__card__desc'>
                             при хорошей страховой истории стоимость следующих страховок снижается
                         </p>
                     </div>
-                    <div className="bonus__card">
-                        <h3 className="bonus__card__title">
-                            1 рубль
-                        </h3>
-                        <p className="bonus__card__desc">
-                            за покупку первой страховки
-                        </p>
+                    <div className='bonus__card'>
+                        <h3 className='bonus__card__title'>1 рубль</h3>
+                        <p className='bonus__card__desc'>за покупку первой страховки</p>
                     </div>
-                    <div className="bonus__card">
-                        <h3 className="bonus__card__title">
-                            Приведи друга
-                        </h3>
-                        <p className="bonus__card__desc">
-                            и получи страховку бесплатно
-                        </p>
+                    <div className='bonus__card'>
+                        <h3 className='bonus__card__title'>Приведи друга</h3>
+                        <p className='bonus__card__desc'>и получи страховку бесплатно</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Bonus;
--- src/components/Сhat.jsx
+++ src/components/Сhat.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 import chatLogo from '../images/chat-logo.png';
 import sendIcon from '../images/send-icon.svg';
@@ -8,40 +8,39 @@ import chat from '../images/chat.svg';
 const Chat = () => {
     const [open, setOpen] = useState(false);
     const [query, setQuery] = useState('');
-    const hs =  window.localStorage.getItem('history');
+    const hs = window.localStorage.getItem('history');
     const [history, setHistory] = useState(hs ? JSON.parse(hs) : []);
 
     const onSubmit = (event) => {
         event.preventDefault();
         setHistory((prev) => {
-            let updatedHistory = [...prev, {type: "user", text: query}];
+            let updatedHistory = [...prev, { type: 'user', text: query }];
             if (updatedHistory.length > 50) {
-                updatedHistory = updatedHistory.slice(-50);
+                updatedHistory = updatedHistory.slice(-2);
             }
             window.localStorage.setItem('history', JSON.stringify(updatedHistory));
             return updatedHistory;
         });
-        let q = " " + query;
-        setQuery("");
+        let q = ' ' + query;
+        setQuery('');
         fetch('https://api.dino-misis.ru/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                text: history.map(item => item.type === 'user' ? item.text : "").join(" ") + q
-            })
-
+                text: history.map((item) => (item.type === 'user' ? item.text : '')).join(' ') + q,
+            }),
         })
-            .then(response => {
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 return response.json();
             })
-            .then(data => {
+            .then((data) => {
                 setHistory((prev) => {
-                    let updatedHistory = [...prev, {type: "bot", text: data.text}];
+                    let updatedHistory = [...prev, { type: 'bot', text: data.text }];
                     if (updatedHistory.length > 50) {
                         updatedHistory = updatedHistory.slice(-50);
                     }
@@ -49,56 +48,59 @@ const Chat = () => {
                     return updatedHistory;
                 });
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('There was a problem with your fetch operation:', error);
             });
     };
 
     const messages = history.map((item, i) => {
-        if (item.type === "user") {
+        if (item.type === 'user') {
             return (
-                <div key={i} className="user__message">
+                <div key={i} className='user__message'>
                     {item.text}
                 </div>
-            )
-        } else if (item.type === "bot") {
+            );
+        } else if (item.type === 'bot') {
             return (
-                <div key={i} className="bot__message">
+                <div key={i} className='bot__message'>
                     {item.text}
                 </div>
-            )
+            );
         }
     });
 
     return (
-        <div className="chat">
-            {open ?
-                <div className="chat__client">
-                    <div className="chat__header">
-                        <div className="chat__header__title">
-                            <p className="chat__header__subtitle">ДиноБот</p>
+        <div className='chat'>
+            {open ? (
+                <div className='chat__client'>
+                    <div className='chat__header'>
+                        <div className='chat__header__title'>
+                            <p className='chat__header__subtitle'>ДиноБот</p>
                             <p>Поддержка RUSSPASS</p>
                         </div>
-                        <img src={chatLogo} alt="Russ Pass"/>
+                        <img src={chatLogo} alt='Russ Pass' />
                     </div>
-                    <div className="dialog">
-                        {messages}
-                    </div>
-                    <form className="chat__form" onSubmit={onSubmit}>
-                        <button type="button" className="chat__form__clip"><img src={clip} alt=""/></button>
-                        <textarea placeholder="Опишите свой вопрос в одном сообщении" value={query}
-                                  onChange={(e) => setQuery(e.target.value)}/>
-                        <button className="chat__form__submit" type="submit">
-                            <img src={sendIcon} alt=""/>
+                    <div className='dialog'>{messages}</div>
+                    <form className='chat__form' onSubmit={onSubmit}>
+                        <button type='button' className='chat__form__clip'>
+                            <img src={clip} alt='' />
+                        </button>
+                        <textarea
+                            placeholder='Опишите свой вопрос в одном сообщении'
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <button className='chat__form__submit' type='submit'>
+                            <img src={sendIcon} alt='' />
                         </button>
                     </form>
                 </div>
-                : null}
-            <button className="chat__button" type="button" onClick={() => setOpen(!open)}>
-                <img src={chat} alt=""/>
+            ) : null}
+            <button className='chat__button' type='button' onClick={() => setOpen(!open)}>
+                <img src={chat} alt='' />
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default Chat;
